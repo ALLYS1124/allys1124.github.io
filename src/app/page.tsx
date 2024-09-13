1435aeb7f72e89fcee33fe643e48f86f8776970c
Ally Santana <60159822+ALLYS1124@users.noreply.github.com>
2024-09-12 22:09:54 -0700

    Coming soon tag
    added "allys website coming soon"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+
+     <main className={styles.main}>
       <div className={styles.description}>
 
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <p> Ally's website is coming soon...</p>
       </div>
+  
 
 
       <div className={styles.grid}>
